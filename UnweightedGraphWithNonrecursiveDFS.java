9fd5038fd18125975d2a169a59e2e8dbbf80a0e1
Dylan Whitehurst <whidyldev@gmail.com>
2019-11-22 16:56:47 -0700

    Add files via upload

--- UnweightedGraphWithNonrecursiveDFS.java
+++ UnweightedGraphWithNonrecursiveDFS.java
@@ -1,5 +1,53 @@
 package CH28;
 
-public class UnweightedGraphWithNonrecursiveDFS<V> extends  UnweightedGraph<V> {
+/*
+Tree dfs(vertex v) {
+    push v into the stack;
+
+    while (the stack is not empty) {
+        pop a vertex, say u, from the stack
+        visit u;
+        mark u visited;
+        for each neighbor w of u if (w has not been visited)
+        push w into the stack;
+    }
+ }
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class UnweightedGraphWithNonrecursiveDFS<V> extends UnweightedGraph<V> {
+    public UnweightedGraphWithNonrecursiveDFS(V[] vertices, int[][] edges) {
+        super(vertices, edges);
+    }
+
+    /** non-recursive method for DFS search
+     * @return*/
+    public SearchTree dfs(V v) {
+        boolean[] isVisited = new boolean[vertices.size()];
+        int[] parent = new int[vertices.size()];
+        for (int i = 0; i < parent.length; i++)
+            parent[i] = -1; // Initialize parent[i] to -1
+        Stack<V> vertexStack = new Stack<V>();
+        List<Integer> searchOrder = new ArrayList<>();
+        vertexStack.push(v);
+        isVisited[getIndex(v)] = true; // vertex v is on the stack so it is visited
+
+
+        while(!vertexStack.empty()) {
+            V currentlyVisiting = vertexStack.pop();
+            searchOrder.add(getIndex(currentlyVisiting));
+            for (int neighborIndex : getNeighbors(getIndex(currentlyVisiting))){
+                if(!isVisited[neighborIndex]) {
+                    parent[neighborIndex] = getIndex(currentlyVisiting);
+                    vertexStack.push(getVertex(neighborIndex));
+                    isVisited[neighborIndex] = true;
+                }
+            }
+        }
+        return new SearchTree(getIndex(v), parent, searchOrder);
+    }
 
 }
